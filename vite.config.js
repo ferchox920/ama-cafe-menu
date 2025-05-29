2eda1911a749f359993a558673f2b1773e26a979
ferchox920 <fernandoramones92@gmail.com>
2025-05-29 15:33:00 -0300

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/ama-cafe-menu/',
 
+  base: '/ama-cafe-menu/', 
 })
